a8954f22878726f915e96520837fae33d37ca61a
hbally <hbally@student.42.fr>
2020-02-19 11:19:13 +0100

    Rename env variables

--- backend/config/config.go
+++ backend/config/config.go
@@ -8,19 +8,19 @@ import (
 )
 
 type Config struct {
+	Env string `yaml:"env" envconfig:"MIMIKO_ENV"`
 	Server struct {
-		Host string `yaml:"host" envconfig:"NU_SERVER_HOST"`
-		Port int    `yaml:"port" envconfig:"NU_SERVER_PORT"`
+		Host string `yaml:"host" envconfig:"MIMIKO_SERVER_HOST"`
+		Port int    `yaml:"port" envconfig:"MIMIKO_SERVER_PORT"`
 	} `yaml:"server"`
 	Auth struct {
-		JwtKey string `yaml:"jwt_key" envconfig:"NU_JWT_KEY"`
+		JwtKey string `yaml:"jwt_key" envconfig:"MIMIKO_JWT_KEY"`
 	} `yaml:"auth"`
 	Spotify struct {
 		ClientId     string `yaml:"client_id" envconfig:"SP_CLIENT_ID"`
 		ClientSecret string `yaml:"client_secret" envconfig:"SP_CLIENT_SECRET"`
 		RedirectUri  string `yaml:"redirect_uri" envconfig:"SP_REDIRECT_URI"`
 	} `yaml:"spotify"`
-	Env string `yaml:"env" envconfig:"NU_ENV"`
 }
 
 func New() *Config {
