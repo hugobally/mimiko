6ecf92edbbef95dd83ca8428bd1919105f9c6bbf
Hugo Bally <bally.hugo@gmail.com>
2020-02-20 16:57:15 +0100

    Add catchall handler to static file server to accomodate Vue app

--- backend/static/handler.go
+++ backend/static/handler.go
@@ -7,10 +7,42 @@ import (
 
 type Handler struct{ *shared.Services }
 
-func (h *Handler) SetupRoutes(mux *http.ServeMux) {
-	mux.Handle("/", http.FileServer(http.Dir(h.Config.Server.StaticPath)))
-}
-
 func NewHandler(s *shared.Services) *Handler {
 	return &Handler{s}
+}
+
+func (h *Handler) SetupRoutes(mux *http.ServeMux) {
+	mux.Handle("/", WrapNotFound(http.FileServer(http.Dir(h.Config.Server.StaticPath))))
+}
+
+// Vue Frontend requires 404's on static assets to return index.html instead
+func WrapNotFound(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cw := &CustomResponseWriter{ResponseWriter: w}
+		next.ServeHTTP(w, r)
+		if cw.status == http.StatusNotFound {
+			http.ServeFile(w, r, "/")
+		}
+	}
+}
+
+type CustomResponseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *CustomResponseWriter) WriteHeader(status int) {
+	if status != http.StatusNotFound {
+		w.ResponseWriter.WriteHeader(status)
+	} else {
+		w.status = status
+	}
+}
+
+func (w *CustomResponseWriter) Write(p []byte) (int, error) {
+	if w.status != http.StatusNotFound {
+		return w.ResponseWriter.Write(p)
+	} else {
+		return len(p), nil
+	}
 }
