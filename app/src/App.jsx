0928a124ac1b364d12c84120f8555adf7b9a3c59
Hugo Bally <hugo.bally@pretto.fr>
2025-05-15 13:17:58 +0200

    start rewrite

---
+++ app/src/App.jsx
@@ -0,0 +1,19 @@
+import { Stage, Container, Graphics } from '@pixi/react';
+
+function App() {
+  return (
+    <Stage width={800} height={600} options={{ backgroundColor: 0xFFFFFF }}>
+      <Container>
+        <Graphics
+          draw={(g) => {
+            g.beginFill(0x000000);
+            g.drawCircle(400, 300, 50);
+            g.endFill();
+          }}
+        />
+      </Container>
+    </Stage>
+  );
+}
+
+export default App;
